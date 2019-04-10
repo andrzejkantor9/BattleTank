c4a0fe1e43c3067fe6840aa3adf51fa7336437d2
andrzejkantor9 <anderzejn@gmail.com>
2019-04-10 10:22:10 +0200

    override intro

--- BattleTank/Source/BattleTank/Private/TankPlayerControllerMain.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerControllerMain.cpp
@@ -7,3 +7,19 @@ ATank* ATankPlayerControllerMain::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+void ATankPlayerControllerMain::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player is possesing: %s"), *(ControlledTank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("No possesed player tank."));
+	}
+}

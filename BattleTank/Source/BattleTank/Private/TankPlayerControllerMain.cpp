b72062b4c3b685363fbacad1fd159a48048675fd
andrzejkantor9 <anderzejn@gmail.com>
2019-04-15 21:05:13 +0200

    playing with include order (possible bugs here)

--- BattleTank/Source/BattleTank/Private/TankPlayerControllerMain.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerControllerMain.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Tank.h"
 #include "../public/TankPlayerControllerMain.h"
 #include "BattleTank.h"
 #include "GameFramework/Controller.h"

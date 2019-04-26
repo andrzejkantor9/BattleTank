0a35ca8e50fb2eb07d7ac905c92db214f96f71bf
andrzejkantor9 <anderzejn@gmail.com>
2019-04-26 22:24:11 +0200

    friction and movement working

--- BattleTank/Source/BattleTank/Private/TankTracks.cpp
+++ BattleTank/Source/BattleTank/Private/TankTracks.cpp
@@ -9,4 +9,8 @@ void UTankTracks::SetThrottle(float Throttle)
 	UE_LOG(LogTemp, Warning, TEXT("At %f projectile is fired at %f speed."), *Name, Throttle);
 
 	//TODO clamp actual throttle value so player can't over-drive
+	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>( GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }

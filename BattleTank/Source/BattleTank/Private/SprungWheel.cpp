b47fc5617bac0beb377fdc523007f09e32abdf3d
andrzejkantor9 <anderzejn@gmail.com>
2019-06-19 00:23:29 +0200

    applying only forward and backward rotation on wheels working properly

--- BattleTank/Source/BattleTank/Private/SprungWheel.cpp
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -13,38 +13,42 @@ ASprungWheel::ASprungWheel()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Mass Wheel Constraint"));
+	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("MassWheelConstraint"));
 	SetRootComponent(MassWheelConstraint);
 
 	Axle = CreateDefaultSubobject<USphereComponent>(FName("Axle"));
-	Axle->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
-	Axle->SetNotifyRigidBodyCollision(true);
-	Axle->SetVisibility(true);
-
-	Axle->SetSimulatePhysics(true);
-	Axle->SetEnableGravity(true);
-	Axle->SetMassOverrideInKg(NAME_None, 400000.f, true);
+	Axle->SetupAttachment(MassWheelConstraint);
 
 	Wheel = CreateDefaultSubobject<USphereComponent>(FName("Wheel"));
-	//Wheel->AttachToComponent(Axle, FAttachmentTransformRules::KeepRelativeTransform);
 	Wheel->SetupAttachment(Axle);
-	Wheel->SetNotifyRigidBodyCollision(true);
-	Wheel->SetVisibility(true);
-
-	Wheel->SetSimulatePhysics(true);
-	Wheel->SetEnableGravity(true);
-	Wheel->SetMassOverrideInKg(NAME_None, 400000.f, true);
 
 	AxleWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("AxleWheelConstraint"));
-	//AxleWheelConstraint->AttachToComponent(Axle, FAttachmentTransformRules::KeepRelativeTransform);
 	AxleWheelConstraint->SetupAttachment(Axle);
 
-	MassWheelConstraint->SetLinearZLimit(ELinearConstraintMotion::LCM_Free, 1.f);
-	MassWheelConstraint->SetAngularTwistLimit(EAngularConstraintMotion::ACM_Locked, 1.f);
-	MassWheelConstraint->SetAngularSwing1Limit(EAngularConstraintMotion::ACM_Locked, 1.f);
-	MassWheelConstraint->SetAngularSwing2Limit(EAngularConstraintMotion::ACM_Locked, 1.f);
-	MassWheelConstraint->SetLinearPositionDrive(false, false, true);
-	MassWheelConstraint->SetLinearVelocityDrive(false, false, true);
+	//Axle->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	//Axle->SetNotifyRigidBodyCollision(true);
+	//Axle->SetVisibility(true);
+
+	//Axle->SetSimulatePhysics(true);
+	//Axle->SetEnableGravity(true);
+	//Axle->SetMassOverrideInKg(NAME_None, 400000.f, true);
+
+	//Wheel->AttachToComponent(Axle, FAttachmentTransformRules::KeepRelativeTransform);
+	//Wheel->SetNotifyRigidBodyCollision(true);
+	//Wheel->SetVisibility(true);
+
+	//Wheel->SetSimulatePhysics(true);
+	//Wheel->SetEnableGravity(true);
+	//Wheel->SetMassOverrideInKg(NAME_None, 400000.f, true);
+
+	//AxleWheelConstraint->AttachToComponent(Axle, FAttachmentTransformRules::KeepRelativeTransform);
+
+	//MassWheelConstraint->SetLinearZLimit(ELinearConstraintMotion::LCM_Free, 1.f);
+	//MassWheelConstraint->SetAngularTwistLimit(EAngularConstraintMotion::ACM_Locked, 1.f);
+	//MassWheelConstraint->SetAngularSwing1Limit(EAngularConstraintMotion::ACM_Locked, 1.f);
+	//MassWheelConstraint->SetAngularSwing2Limit(EAngularConstraintMotion::ACM_Locked, 1.f);
+	//MassWheelConstraint->SetLinearPositionDrive(false, false, true);
+	//MassWheelConstraint->SetLinearVelocityDrive(false, false, true);
 
 	//MassWheelConstraint->SetConstrainedComponents(Cast<UPrimitiveComponent>(GetAttachParentActor()), NAME_None, Cast<UPrimitiveComponent>(Wheel), NAME_None);
 }
@@ -55,8 +59,6 @@ void ASprungWheel::BeginPlay()
 	Super::BeginPlay();
 
 	SetupConstraint();
-
-	//AttachToComponent();
 }
 
 void ASprungWheel::SetupConstraint()
@@ -64,6 +66,26 @@ void ASprungWheel::SetupConstraint()
 	if (!GetAttachParentActor()) { return; }
 	UPrimitiveComponent* BodyRoot = Cast<UPrimitiveComponent>(GetAttachParentActor()->GetRootComponent());
 	if (!BodyRoot) { return; }
+	if (!Axle) 
+	{
+		UE_LOG(LogTemp, Error, TEXT("Axle not present."));
+		return;
+	}
+	if (!Wheel)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Wheel not present."));
+		return;
+	}
+	if (!MassWheelConstraint)
+	{
+		UE_LOG(LogTemp, Error, TEXT("MassWheelConstraint not present."));
+		return;
+	}
+	if (!AxleWheelConstraint)
+	{
+		UE_LOG(LogTemp, Error, TEXT("AxleWheelConstraint not present."));
+		return;
+	}
 	MassWheelConstraint->SetConstrainedComponents(BodyRoot, NAME_None, Axle, NAME_None);
 	AxleWheelConstraint->SetConstrainedComponents(Axle, NAME_None, Wheel, NAME_None);
 }
